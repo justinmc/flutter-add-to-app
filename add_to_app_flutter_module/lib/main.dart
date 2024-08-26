3852f2ab759fdd0ba250c73c3881398c6b5f0b5c
Justin McCandless <jmccandless@google.com>
2024-08-26 15:40:53 -0700

    Add predictive back page transitions, but they dont seem to do anything. It gets handlePopRoute

--- add_to_app_flutter_module/lib/main.dart
+++ add_to_app_flutter_module/lib/main.dart
@@ -13,6 +13,11 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: <TargetPlatform, PageTransitionsBuilder>{
+            TargetPlatform.android: PredictiveBackPageTransitionsBuilder(),
+          },
+        )
       ),
       initialRoute: '/',
       routes: {
